1ebf592376f7a13fa7164bc3eacea825f6a5e77f
Adrien Loup <adloup@gmail.com>
2024-12-07 02:14:08 +0100

    fix(config): add alias to fix path links

--- src/generic/components/Header/Header.tsx
+++ src/generic/components/Header/Header.tsx
@@ -1,9 +1,8 @@
-import styles from "@/generic/components/header/Header.module.scss";
+//import styles from "@/generic/components/header/Header.module.scss";
 
 export const Header = () => {
   return (
-    <header className={styles.header} role="banner">
-      header
-    </header>
+    // <header className={styles.header} role="banner">
+    <header role="banner">header</header>
   );
 };
