2736cb0da83bf45b9b3da21c1b02817c5640457b
Adrien Loup <adloup@gmail.com>
2024-12-07 02:11:23 +0100

    fix(config): add alias to fix path links

--- src/game/page/Game.tsx
+++ src/game/page/Game.tsx
@@ -5,7 +5,7 @@ import { Production } from "@/game/components/production/Production";
 // import { Header } from "../../generic/components/header/Header";
 
 import styles from "@/game/page/Game.module.scss";
-import { Button } from "@/game/button/Button";
+import { Button } from "@/button/Button";
 
 function Game() {
   return (
